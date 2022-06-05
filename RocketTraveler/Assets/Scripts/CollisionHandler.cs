246bd129f50d96db56f356582f0bcb97576de3f6
MongooseFox <jakstroinski@gmail.com>
2022-06-05 21:53:36 +0200

    main

--- RocketTraveler/Assets/Scripts/CollisionHandler.cs
+++ RocketTraveler/Assets/Scripts/CollisionHandler.cs
@@ -2,23 +2,66 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip crash;
+    [SerializeField] ParticleSystem particleSuccess;
+    [SerializeField] ParticleSystem particleCrash;
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
      void OnCollisionEnter(Collision other) 
     {
+        if(isTransitioning) { return; }
+        
         switch (other.gameObject.tag)  
         {
             case "Friendly":
                 Debug.Log("This thing is friendly");
                 break;
             case "Finish":
-                Debug.Log("This is finish");
-                break;
-            case "Fuel":
-                Debug.Log("You picked up fuel");
+                StartSuccesSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+    void StartSuccesSequence()
+    {
+        //todo add sfx upon success
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        //todo add particle effect upon success
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    void StartCrashSequence()
+    {
+        //todo add sfx upon crash
+        audioSource.Stop();
+        isTransitioning = true;
+        audioSource.PlayOneShot(crash);
+        //todo add particle effect upon crash
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+
     }
     void ReloadLevel()
     {
